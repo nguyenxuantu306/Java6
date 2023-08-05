994eae5764c3ab13fc800a826134b03344c36516
Nghiahtps20692 <100018787+Nghiahtps20692@users.noreply.github.com>
2023-08-05 14:18:48 +0700

    ++

--- src/main/resources/static/assets/admin/admin-app.js
+++ src/main/resources/static/assets/admin/admin-app.js
@@ -26,7 +26,7 @@ app.config(function($routeProvider){
 		controller:"authority-ctrl"
 	})	
 	.when("/unauthorized",{
-		templateUrl:"/assets/admin/authority/404.html",
+		templateUrl:"/assets/admin/authority/index.html",
 		controller:"authority-ctrl"
 	})	
 	.when("/main",{

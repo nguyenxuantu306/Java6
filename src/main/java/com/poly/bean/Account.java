20e4118273bfdbd5037f88cd6afc06073c76d9c4
HungNVCODE-GITHUB-2003 <hungcode2003@gmail.com>
2023-08-09 20:56:50 +0700

    New

--- src/main/java/com/poly/bean/Account.java
+++ src/main/java/com/poly/bean/Account.java
@@ -36,35 +36,26 @@ public class Account implements Serializable {
 
 	@Id	
 	String Id;
-	@NotNull(message = "Không được để trống Tài khoản")
-	@Pattern(regexp = "^[a-zA-Z0-9](_(?!(\\.|_))|\\.(?!(_|\\.))|[a-zA-Z0-9]){0,}[a-zA-Z0-9]$", message = "Tài khoản không đúng định dạng")
-	@Size(min = 6, max = 18, message = "Tài khoản phải có độ dài từ 6 - 18 ký tự")
+	
 	String username;
 	
-	@NotBlank(message = "Không được để trống Mật khẩu")
-	@Size(min = 4, max = 50, message = "Mật khẩu phải có độ dài từ 4 - 50 ký tự")
+	
 	String Password;
 	
-	@NotNull(message = "Không được để trống Họ tên")
-	@Pattern(regexp = "^\\S([a-zA-Z\\xC0-\\uFFFF]{0,}[ \\-\\']{0,}){1,}$", message = "Họ tên không đúng định dạng")
-	@Size(min = 4, max = 50, message = "Họ tên phải có độ dài từ 4 - 50 ký tự")
+	
 	String Fullname;
 	
-	@NotNull(message = "Không được để trống Email")
-	@Pattern(regexp = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$", message = "Email không đúng dịnh dạng")
+	
 	String Email;
 	
 	String Photo;
 	
 	Boolean Gender;
 	
-	@NotNull(message = "Không được để trống Địa chỉ")
-	@Pattern(regexp = "^\\S([a-zA-Z0-9\\xC0-\\uFFFF\\.]{0,}[ \\-\\' \\.-]{0,}){1,}$", message = "Địa chỉ không đúng định dạng")
-	@Size(min = 4, max = 100, message = "Địa chỉ phải có độ dài từ 4 - 100 ký tự")
+	
 	String Address;
 	
-	@NotBlank(message = "Không được để trống số điện thoại")
-	@Pattern(regexp = "(84|0[3|5|7|8|9])+([0-9]{8})\\b", message = "Số điện thoại không đúng định dạng")
+	
 	String Phone;
 	
 	@Temporal(TemporalType.DATE)
